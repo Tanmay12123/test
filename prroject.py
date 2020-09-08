a5485f70afdb7cf70652d8458be0219b0d2191fc
unknown <automatrix888@gmail.com>
2020-09-08 13:26:59 +0530

    my first try and except code

--- prroject.py
+++ prroject.py
@@ -1,1 +1,8 @@
-print("Hello Dude")
+try:   
+    x = int(input("Age:  "))
+    if x > 100: 
+        print("Put your real age not the fake one...")
+    print(x)
+except ValueError:
+    print("I think the age only contains numbers why are you entering strings?")
+    
