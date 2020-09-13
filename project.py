b4970a1d0f256f15c54047b1f42176cd452e0739
unknown <automatrix888@gmail.com>
2020-09-13 18:43:14 +0530

    calculator not perfectly woring

--- project.py
+++ project.py
@@ -1,2 +1,38 @@
-HU
 
+
+def calculator(first_number,operators,second_number):
+    
+    print("Hello welcome to MDs calculator\n Type the operators according to your needs")
+    first_number = int(input("Enter the 1 number:  "))
+    operators = input("Enter your operator:  ")
+    second_number = int(input("Enter the 2 number: "))
+    
+    if operators == "+":
+        print(first_number+second_number)
+        
+        
+    elif operators == "-":
+        print(first_number -second_number)
+        
+    elif operators == "*":
+        print(first_number *second_number)
+        
+    elif operators == "/":
+        print(first_number /second_number)
+    
+    else:
+        print("Sorry, we don't understand this operator")
+               
+print("Thank you for using the calculator")
+user_choice =input("Please enter y/n to play again or quit:   ")
+
+if user_choice== "y":
+    print("yes!!")
+
+elif user_choice== "n":
+    print("Goodbye!     Meet you again")
+    break
+
+else:
+    print("sorry I don't understand'")
+    
--- tanmay.js
+++
@@ -1,4 +0,0 @@
-// hi, dud this is my first time on java script
-console.log('Hello World');
-let hi = 'Hello dude!';
-console.log(hi);
