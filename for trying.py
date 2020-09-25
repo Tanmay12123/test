cfd141747fa289d628775ee5d525ad3bcbe55dd7
unknown <automatrix888@gmail.com>
2020-09-25 22:41:01 +0530

    led turning on and off

--- for trying.py
+++ for trying.py
@@ -1,25 +1,28 @@
+light_state=True
 
-light_state = False
+user_value=""
+# while 1==1:
 
-while 1==1:
-   
-    user_value = input("value:  ")
+def checker():
     if user_value == "ON" or user_value=="on":
         print("Turning on the light")
         light_state =True
+        print("Samosa")
         # pass
         #if light_state == True:
             #print("Light is aldready switched on") 
     elif user_value == "OFF" or   user_value == "off":
         print("Turning off the light")
         light_state = False
+        print("samosa false")
         #if light_state == False:
         # print("Light is aldready switched off")
     else:
         print("Sorry, that was an invalid command!")
-        pass
+        # pass
+    return light_state
         
-        
-    # for i in range(10):
-    print("The current state of the light is:", light_state)
-        # user_input()
+for i in range(10):
+    user_value = input("value:  ")
+    print("The current state of the light is:", checker())
+    # user_input()
--- project2.py
+++ project2.py
@@ -1,2 +1,19 @@
-x = [12,4,54,78,54,32,45,66,99]
-print(sorted(x))
+user_value = input("value:  ")
+light_state = False
+
+
+if user_value == "ON"or "on":
+    if light_state == True:
+        print("Light is aldready switched on")
+    print("Turning on the light")
+    light_state =True
+         
+elif user_value == "OFF"or "of":
+    print("Turning off the light")
+    light_state =False
+    if light_state == False:
+        print("Light is aldready switched off")
+    
+for i in range(1):
+    print(light_state)
+    
